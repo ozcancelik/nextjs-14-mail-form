3c618b6c2bf95f8671dbd171583f3664e5160170
Özcan Çelik <goreality@gmail.com>
2024-05-06 22:25:43 +0300

    Update form texts.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
 import Form from "@/components/form";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="p-10">
-      <div className="w-1/2 mx-auto">
+    <div>
+      <Head>
+        <title>Next.js Mail Form Sender Example with Server Actions</title>
+      </Head>
+      <div className="md:w-1/2 w-5/6 p-5 border mx-auto my-5 bg-slate-200 rounded-lg">
+        <h1 className="text-xl font-bold">Next.js Mail Form Sender Example</h1>
+        <p className="mb-12">
+          This is an example of a mail form sender with server actions.
+          <br />
+          It uses Nodemailer, Zod, React Hook Form, Handlebars, ReCAPTCHA and
+          TailwindCSS. Shadcn/UI is used for Toast messages.
+          <br />
+          <br />
+          You can find the code for this example{" "}
+          <Link
+            className="font-bold underline"
+            href="https://github.com/ozcancelik/nextjs-14-mail-form"
+            target={"_blank"}
+          >
+            here
+          </Link>
+        </p>
         <Form />
       </div>
     </div>
