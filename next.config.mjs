42862f7c039d5b0b21db6e8549707b6ad46a8979
Özcan Çelik <goreality@gmail.com>
2025-11-19 11:28:23 +0300

    refactor: update webpack to turbopack in next.config and adjust tailwind config formatting

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     // This is for the require.extensions warning in handlebars 
-    webpack: (config) => {
+    turbopack: (config) => {
         config.resolve.alias = {
             ...config.resolve.alias,
             'handlebars': 'handlebars/dist/handlebars.js',
